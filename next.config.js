1b0c092db060f741ed6e1e2d4f34d6be476c9a95
Milton Boos Junior <junior_boos@live.com>
2021-01-09 18:15:39 +0000

    changed next config

--- next.config.js
+++ next.config.js
@@ -1,11 +1,10 @@
 const withImages = require("next-images");
-module.exports = withImages({ esModule: true });
 
 const isProd = process.env.NODE_ENV === "production";
 
-module.exports = {
-  // Use the CDN in production and localhost for development.
+module.exports = withImages({
+  esModule: true,
   assetPrefix: isProd
     ? "https://cdn.statically.io/gh/juniorboos.github.io/portfolio/"
     : "",
-};
+});
